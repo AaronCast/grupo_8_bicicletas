9e47c8e2898e520f8c5cbb70c4a221eec9b1ebb1
Israel <israeljonathanroma@outlook.com>
2021-08-16 14:57:07 -0500

    cambios Israel

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const publicPath= path.resolve(__dirname,'./public') ;
+app.use(express.static(publicPath));
+
+
 
 /* Home page */
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'./views/index.html'));
 });
-
+/* Home page */
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+});
 
 
 app.listen(3030, () => 
