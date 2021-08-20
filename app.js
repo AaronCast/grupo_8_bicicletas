f2a8fb88f50da58c8db95ea68c9b127b2d634031
irosillo <87401235+irosillo@users.noreply.github.com>
2021-08-19 23:32:13 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static('public'));
+
+/* Home page */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/index.html'));
+});
+
+
+/* ProductCar */
+app.get('/productdetail', (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/productDetail.html'));
+});
+
+/* Login */
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/login.html'));
+});
+/* Formulario Registro */
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+});
+/* Carrito de Compras */
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/productCart.html'));
+});
+app.listen(3030, () => 
+console.log('Corriendo'));
