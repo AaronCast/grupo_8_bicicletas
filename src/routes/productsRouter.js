714efcf280350c49b7356c6ce2e71c6001838c50
Aarón <castelan_aaron@yahoo.com>
2021-09-17 01:41:57 -0500

    Multer

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
 const productsController = require('../controllers/productsController');
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null, 'public/img/products')
+    },
+    filename: function(req,file,cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({storage: storage});
+
 
 router.get('/', productsController.index);
 router.get('/products', productsController.index);
 router.get('/detail/:id', productsController.details);
 router.get('/create-product', productsController.viewCreate);
-router.put('/create-product', productsController.create);
+router.put('/create-product', upload.any() , productsController.create);
 
 
 module.exports = router;
