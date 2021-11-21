af0aacc2983fad58ec10d45f65a930dc46c657a3
AaronCast <castelan_aaron@yahoo.com>
2021-11-20 19:56:25 -0600

    modificando estilos del login y user-profile

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,39 @@
 window.addEventListener('load', (e) => {
     let indexArrow = document.querySelector('.more');
-    let indexMenu = document.querySelector('.submenu ul');
+    let indexMenu = document.querySelector('.topList');
+    let logoProfile = document.querySelector('.logoProfile');
+    let userImageProfile = document.querySelector('.userImageProfile');
+    let userSection = document.querySelector('.userSection');
+    let loginSection = document.querySelector('.loginSection');
 
-    indexMenu.style.display = 'none'
-  
+    indexMenu.style.display = 'none';
+    loginSection.style.display = 'none';
+
     
     indexArrow.addEventListener('click', (e) => {
-       
-        let menuOff = indexMenu.style.display = 'none';
-        let menuOn = indexMenu.style.display = 'block'
-        menuOff
-        if(menuOff){
-            menuOn
+
+        if(indexMenu.style.display == 'none'){
+            indexMenu.style.display = 'block'
         }else{
-            menuOff
+            indexMenu.style.display = 'none'
+        }
+    });
+
+    logoProfile.addEventListener('click', (e) => {
+        if(loginSection.style.display == 'none'){
+            loginSection.style.display = 'block'
+        }else{
+            loginSection.style.display = 'none'
         }
         console.log(e)
-        
-        
-    })
-   
+    });
+    // userImageProfile.addEventListener('click', (e) => {
+    //     if(userSection.style.display == 'none'){
+    //         userSection.style.display = 'block'
+    //     }else{
+    //         userSection.style.display = 'none'
+    //     }
+    //     console.log(e)
+
+    // });
 })
